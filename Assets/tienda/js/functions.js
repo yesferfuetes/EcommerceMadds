1a138080b3cc1d8f1a187d4df05d728c045b4022
yesferbany <yesfer.bany@gmail.com>
2022-04-07 12:37:00 -0600

    primer registro

---
+++ Assets/tienda/js/functions.js
@@ -0,0 +1,204 @@
+$(".js-select2").each(function(){
+    $(this).select2({
+        minimumResultsForSearch: 20,
+        dropdownParent: $(this).next('.dropDownSelect2')
+    });
+})
+
+$('.parallax100').parallax100();
+
+$('.gallery-lb').each(function() { // the containers for all your galleries
+    $(this).magnificPopup({
+        delegate: 'a', // the selector for gallery item
+        type: 'image',
+        gallery: {
+            enabled:true
+        },
+        mainClass: 'mfp-fade'
+    });
+});
+
+$('.js-addwish-b2').on('click', function(e){
+    e.preventDefault();
+});
+
+$('.js-addwish-b2').each(function(){
+    var nameProduct = $(this).parent().parent().find('.js-name-b2').html();
+    $(this).on('click', function(){
+        swal(nameProduct, "is added to wishlist !", "success");
+
+        $(this).addClass('js-addedwish-b2');
+        $(this).off('click');
+    });
+});
+
+$('.js-addwish-detail').each(function(){
+    var nameProduct = $(this).parent().parent().parent().find('.js-name-detail').html();
+
+    $(this).on('click', function(){
+        swal(nameProduct, "is added to wishlist !", "success");
+
+        $(this).addClass('js-addedwish-detail');
+        $(this).off('click');
+    });
+});
+
+/*---------------------------------------------*/
+    /*------Button Agregar al Carrito-------*/
+$('.js-addcart-detail').each(function(){
+    var nameProduct = $(this).parent().parent().parent().parent().find('.js-name-detail').html();
+    $(this).on('click', function(){
+        let id = this.getAttribute('id');
+		let cant = document.querySelector('#cant-product').value;
+
+		if(isNaN(cant) || cant < 1){
+			swal("","La cantidad debe ser mayor o igual que 1" , "error");
+			return;
+		}
+
+        let request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+	    let ajaxUrl = base_url+'/Tienda/addCarrito'; 
+	    let formData = new FormData();
+	    formData.append('id',id);
+	    formData.append('cant',cant);
+
+        request.open("POST",ajaxUrl,true);
+	    request.send(formData);
+	    request.onreadystatechange = function(){
+	        if(request.readyState != 4) return;
+	        if(request.status == 200){
+
+                let objData = JSON.parse(request.responseText);
+                if(objData.status){
+                    document.querySelector("#productosCarrito").innerHTML = objData.htmlCarrito;
+                    document.querySelector("#cantCarrito").setAttribute("data-notify",objData.cantCarrito);
+                    swal(nameProduct, "¡Se agrego al carrito!", "success");
+                }else{
+                    swal("", objData.msg , "error");
+                }
+            }
+            return false;
+        }
+        
+        
+    });
+});
+
+$('.js-pscroll').each(function(){
+    $(this).css('position','relative');
+    $(this).css('overflow','hidden');
+    var ps = new PerfectScrollbar(this, {
+        wheelSpeed: 1,
+        scrollingThreshold: 1000,
+        wheelPropagation: false,
+    });
+
+    $(window).on('resize', function(){
+        ps.update();
+    })
+});
+
+   /*==================================================================
+    [ +/- num product ]*/
+    /* $('.btn-num-product-down').on('click', function(){
+        let numProduct = Number($(this).next().val());
+        let idpr = this.getAttribute('idpr');
+        if(numProduct > 1) $(this).next().val(numProduct - 1);
+        let cant = $(this).next().val();
+
+		if(idpr != null){
+			fntUpdateCant(idpr,cant);
+		}  
+    });
+
+    $('.btn-num-product-up').on('click', function(){
+        let numProduct = Number($(this).prev().val());
+        let idpr = this.getAttribute('idpr');
+        $(this).prev().val(numProduct + 1);
+        let cant = $(this).prev().val();
+		
+        if(idpr != null){
+			fntUpdateCant(idpr,cant);
+		} 
+    }); */
+
+/*---- Actualizar producto escribiendo el numero. -----*/
+/* if(document.querySelector(".num-product")){
+	let inputCant = document.querySelectorAll(".num-product");
+	inputCant.forEach(function(inputCant) {
+		inputCant.addEventListener('keyup', function(){
+			let idpr = this.getAttribute('idpr');
+			let cant = this.value;
+			if(idpr != null){
+		    	fntUpdateCant(idpr,cant);
+		    }
+		});
+	});
+} */
+
+/*------Eliminar un elemento del Modal Carrito------*/
+function fntdelItem(element){
+	//Option 1 = vista Modal
+	//Option 2 = Vista Carrito
+	let option = element.getAttribute("op");
+	let idpr = element.getAttribute("idpr");
+	if(option == 1 || option == 2 ){
+
+		let request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+	    let ajaxUrl = base_url+'/Tienda/delCarrito'; 
+	    let formData = new FormData();
+	    formData.append('id',idpr);
+	    formData.append('option',option);
+
+        //Abriendo conexion
+	    request.open("POST",ajaxUrl,true);
+	    request.send(formData);
+	    request.onreadystatechange = function(){
+	        if(request.readyState != 4) return;
+	        if(request.status == 200){
+	        	let objData = JSON.parse(request.responseText);
+	        	if(objData.status){
+			            document.querySelector("#productosCarrito").innerHTML = objData.htmlCarrito;
+                        document.querySelector("#cantCarrito").setAttribute("data-notify",objData.cantCarrito);
+	        	}else{
+	        		swal("", objData.msg , "error");
+	        	}
+	        } 
+	        return false;
+	    }
+
+	}
+}
+
+/*------ Actualizar cant de producto mediante button +/- ------*/
+/* function fntUpdateCant(pro,cant){
+	if(cant <= 0){
+		document.querySelector("#btnComprar").classList.add("notblock");
+	}else{
+		document.querySelector("#btnComprar").classList.remove("notblock");
+		let request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+	    let ajaxUrl = base_url+'/Tienda/updCarrito'; 
+	    let formData = new FormData();
+	    formData.append('id',pro);    
+	   	formData.append('cantidad',cant);
+
+	   	request.open("POST",ajaxUrl,true);
+	    request.send(formData);
+	    request.onreadystatechange = function(){
+	    	if(request.readyState != 4) return;
+	    	if(request.status == 200){
+	    		let objData = JSON.parse(request.responseText);
+	    		if(objData.status){
+	    			let colSubtotal = document.getElementsByClassName(pro)[0];
+	    			colSubtotal.cells[4].textContent = objData.totalProducto;
+	    			document.querySelector("#subTotalCompra").innerHTML = objData.subTotal;
+	    			document.querySelector("#totalCompra").innerHTML = objData.total;
+	    		}else{
+	    			swal("", objData.msg , "error");
+	    		}
+	    	}
+
+	    }
+	}
+	return false;
+} */
---
+++ Controllers/Carrito.php
@@ -0,0 +1,48 @@
+<?php 
+	require_once("Models/TCategoria.php");
+	require_once("Models/TProducto.php");
+	//require_once("Models/TTipoPago.php");
+	//require_once("Models/TCliente.php");
+
+	class Carrito extends Controllers{
+		use TCategoria, TProducto;
+		public function __construct()
+		{
+			parent::__construct();
+			session_start();
+		}
+
+		public function carrito()
+		{
+			$data['page_tag'] = NOMBRE_EMPESA.' - Carrito';
+			$data['page_title'] = 'Carrito de compras';
+			$data['page_name'] = "carrito";
+			$this->views->getView($this,"carrito",$data); 
+		}
+		public function procesarpago()
+		{
+			if(empty($_SESSION['arrCarrito'])){ 
+				header("Location: ".base_url());
+				die();
+			}
+			if(isset($_SESSION['login'])){
+				$this->setDetalleTemp();
+			}
+			$data['page_tag'] = NOMBRE_EMPESA.' - Procesar Pago';
+			$data['page_title'] = 'Procesar Pago';
+			$data['page_name'] = "procesarpago";
+			$data['tiposPago'] = $this->getTiposPagoT();
+			$this->views->getView($this,"procesarpago",$data); 
+		}
+
+		public function setDetalleTemp(){
+			$sid = session_id();
+			$arrPedido = array('idcliente' => $_SESSION['idUser'],
+								'idtransaccion' =>$sid,
+								'productos' => $_SESSION['arrCarrito']
+							);
+			$this->insertDetalleTemp($arrPedido);
+		}
+
+	}
+ ?>
